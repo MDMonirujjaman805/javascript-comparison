c67b99eb04cf2648b541d565d942be54de66c995
MDMonirujjaman805 <monirbinkhaled1@gmail.com>
2024-08-30 08:35:17 +0600

    if-else stucure

--- compare.js
+++ compare.js
@@ -9,3 +9,13 @@ console .log(10==='10');
 console .log(10===10);
 console .log(10!==10.10);
 console .log(10!=='10');
+
+
+
+// if-else stucure 
+// if(condition (applePrice = 300));{
+//   execute code if condition above is true 
+// }
+// else {
+//   execute code if condition above is false 
+// }
